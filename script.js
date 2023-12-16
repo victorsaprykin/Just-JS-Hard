ace9b584e800e1935dfbbb6ed93791b54ef3d23d
Victor Saprykin <victanik@mail.ru>
2023-12-16 14:56:18 +0300

    Очмстил main

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const num = 266219;
- const digitMultiply = (digits) => {
-  let res = 1;
-  for (let i = 0; i < digits.length; i++) {
-    res *= digits[i];
-  }
-   return res;
- };
-
-const resultDigits = digitMultiply(num.toString())
-
-   console.log(resultDigits);
 
 
 
