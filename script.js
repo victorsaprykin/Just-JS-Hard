d5d43015baaec6f69354f26ce708b32f02e6a658
Victor Saprykin <victanik@mail.ru>
2023-12-16 14:54:20 +0300

    Сделал 1-й хард

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const num = 266219;
+const digitMultiply = (digits) => {
+  let res = 1;
+  for (let i = 0; i < digits.length; i++) {
+    res *= digits[i];
+  }
+  return res;
+};
 
+const resultDigits = digitMultiply(num.toString());
 
-
+console.log(resultDigits);
