9f658f8ad88b87d8b35f0eb6f573929396255b19
Victor Saprykin <victanik@mail.ru>
2023-12-20 18:17:33 +0300

    Check hard lesson 04

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+"use strict";
 
 
+let str = "Очень большой текст будет сокращен обязательно";
 
+function getShortText(str) {
+  if (typeof str !== "string") {
+    console.log("Введена не строка!");
+  }
+
+  if (str.trim().length <= 20) return str.trim();
+  return str.trim().substr(0, 30).trim() + "(...)";
+}
+
+getShortText(str);
+
+console.log(getShortText(str));
